c049ca65bc81576d9677b6a32c87c2c3c0c424bf
zmontesd <zmontesd@gmail.com>
2013-07-19 14:37:21 -0700

    create deck and card models and migrations

--- db/migrate/20130719123505_create_cards.rb
+++ db/migrate/20130719123505_create_cards.rb
@@ -1,4 +1,11 @@
 class CreateCards < ActiveRecord::Migration
   def change
+    create_table :cards do |t|
+      t.string     :term
+      t.string     :definition
+      t.references :deck
+
+      t.timestamps
+    end
   end
 end
