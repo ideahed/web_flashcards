3aaa4b2e191fb62646f75c98f5bca69ad3efebf0
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-20 16:11:54 -0700

    CSS test

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,9 +1,12 @@
 post '/start_round' do
   p "~~~~~~~~~~ #{params} ~~~~~~~~~~~~~"
   deck = Deck.find(params[:id])
-  round = current_user.rounds.create(deck_id: deck.id)  
-  @cards = deck.cards
-  @first_card = @cards.first.definition
+  @round = current_user.rounds.create(deck_id: deck.id)  
+  @first_cards = deck.cards.first
 
   erb :round
+end
+
+post '/guess' do
+
 end
