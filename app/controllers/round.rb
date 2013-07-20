ffdb9f24353c57f8bd981212dc567c9b487e3c76
jesseclay <jesseclay@gmail.com>
2013-07-20 15:11:00 -0700

    udpate functionality for the round, plays an entire round

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,27 +1,37 @@
 post '/start_round' do
   deck = Deck.find(params[:id])
-  @round = current_user.rounds.create(deck_id: deck.id)  
-  @first_card = deck.cards.first
+  @round = current_user.rounds.create(deck_id: deck.id)
+  session[:deck_id] = deck.id
+  session[:round_id] = @round.id
+  @current_card = deck.cards.first
 
   erb :round
-end
-
-get '/next_card' do
-
 end
 
 post '/guess' do
   # check term
   card_id = params[:card_id]
-  round_id = params[:round_id]
+  round_id = session[:round_id]
   guess = params[:guess]
 
   @correct = (guess.downcase == Card.find(card_id).term)
   Guess.create(correct: @correct, card_id: card_id, round_id: round_id)
-  @definition = Card.find(card_id).definition;
+  @term = Card.find(card_id).term
   erb :round
 end
 
 get '/next_card' do 
-  erb :round
+  guesses = Guess.where(round_id: session[:round_id])
+  deck = Deck.where(id: session[:deck_id])
+  
+  deck_card_id_array = deck[0].cards.map(&:id)
+  guess_card_id_array = guesses.map(&:card_id)
+  remaining_cards_to_guess = deck_card_id_array - guess_card_id_array
+  next_card_id = remaining_cards_to_guess.first  
+  if next_card_id.nil? 
+    redirect '/user_dashboard'
+  else
+    @current_card = deck[0].cards.find(next_card_id)
+    return erb :round
+  end
 end
