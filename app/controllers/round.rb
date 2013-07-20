1d34129d3cb819c27596a125ba996e1e660ac30d
zmontesd <zmontesd@gmail.com>
2013-07-19 22:35:58 -0700

    add round view and round logic to controller

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,9 +1,23 @@
 post '/start_round' do
-  p "~~~~~~~~~~ #{params} ~~~~~~~~~~~~~"
   deck = Deck.find(params[:id])
-  round = current_user.rounds.create(deck_id: deck.id)  
-  @cards = deck.cards
-  @first_card = @cards.first.definition
+  @round = current_user.rounds.create(deck_id: deck.id)  
+  @first_card = deck.cards.first
 
+  erb :round
+end
+
+post '/guess' do
+  # check term
+  card_id = params[:card_id]
+  round_id = params[:round_id]
+  guess = params[:guess]
+
+  @correct = guess.downcase == Card.find(card_id).term
+  Guess.create(correct: correct, card_id: card_id, round_id: round_id)
+
+  erb :round
+end
+
+get '/next_card' do 
   erb :round
 end
