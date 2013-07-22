65798be7fdc201ba41e39cf65df128e617e7ce4f
zmontesd <zmontesd@gmail.com>
2013-07-21 19:14:38 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
+  before_save { email.downcase! }
   
   has_many :rounds
 
-  validates :user_name, presence: true, uniqueness: true
-  validates :password_hash, presence: true
-  validates :email, presence: true
+  validates :user_name, presence: true,  length: { maximum: 50 }
+  validates :password_hash, presence: true, length: { minimum: 6 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
   include BCrypt
 
- def password
+  def password
     @password ||= Password.new(password_hash)
   end
 
